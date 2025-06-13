d51e0d93e658406e7bffdd799a74fb5dfd219f3d
Matthew Lyon <lyonmatthew2003@hotmail.com>
2025-06-13 16:54:19 +0100

    Optimisation and Usability Improvements
    - Added attributes to restrict configuration values from inspector
    - Added YOLOCustomizer.cs to separate model customization from inference
    - Added YOLO customization options to YOLOHandler
    - New YOLOCustomizationParameters class to provide simple interface for settings attributes in YOLOCustomizer
    - Altered YOLOInferenceHandler constructor implementation to accept model instead of model asset. Model loading is now the responsibility of the calling scope.
    - To facilitiate this, YOLOInferenceHandler now accepts YOLOModel which wraps the Sentis Model class. This is returned from the YOLO customizer, encouraging its use.

--- Assets/YOLO/Scripts/ObjectDetection/YOLOInferenceHandler.cs
+++ Assets/YOLO/Scripts/ObjectDetection/YOLOInferenceHandler.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections;
+using System.IO;
 using Unity.Sentis;
 using UnityEngine;
 using YOLOQuestUnity.Inference;
+using YOLOQuestUnity.ObjectDetection.Utilities;
 
 namespace YOLOQuestUnity.ObjectDetection
 {
@@ -15,21 +17,14 @@ namespace YOLOQuestUnity.ObjectDetection
         private readonly float _scoreThreshold = 0.5f;
 
 
-        public YOLOInferenceHandler(YOLOModelAsset modelAsset, ref int size, YOLOInferenceHandlerParameters parameters)
+        public YOLOInferenceHandler(YOLOModel model, ref int size, BackendType backendType = BackendType.GPUCompute)
         {
-            _model = ModelLoader.Load(modelAsset.ModelAsset);
+            _model = model.Model;
 
             if (_model.inputs[0].shape.Get(2) != -1) size = _model.inputs[0].shape.Get(2);
             _size = size;
 
-            _iouThreshold = parameters.IoUThreshold;
-            _scoreThreshold = parameters.ScoreThreshold;
-
-            if (parameters.AddClassificationHead) AddClassificationHead();
-
-            if (parameters.QuantizeModel) ModelQuantizer.QuantizeWeights(parameters.QuantizationType, ref _model);
-
-            _worker = new Worker(_model, parameters.BackendType);
+            _worker = new Worker(_model, backendType);
             _textureAnalyser = new TextureAnalyser(_worker);
         }
 
@@ -69,107 +64,5 @@ namespace YOLOQuestUnity.ObjectDetection
             return _worker.PeekOutput();
         }
 
-        private void AddClassificationHead()
-        {
-            var graph = new FunctionalGraph();
-
-            var inputs = graph.AddInputs(_model);
-            var output = Functional.Forward(_model, inputs)[0];
-
-            /*
-            Output Format
-
-            1x84xN tensor
-
-            Each of the N columns are a prediction. The first 0..4 rows of each column are the position and scale. The last 4..84 rows are the confidences of each of the 80 classes.
-
-            */
-
-            var centersToCornersData = new[]
-            {
-                1,      0,      1,      0,
-                0,      1,      0,      1,
-                -0.5f,  0,      0.5f,   0,
-                0,      -0.5f,  0,      0.5f
-            };
-            var centersToCorners = Functional.Constant(new TensorShape(4, 4), centersToCornersData); // (4,4)
-            var slicedClasses = output[.., 4..84, ..]; // (1,80,N)
-
-            var argMaxClasses = Functional.ArgMax(slicedClasses, 1, false); // (1,N)
-            var confidences = Functional.ReduceMax(slicedClasses, 1); // (1,N)
-            var slicedPositions = output[.., 0..4, ..]; // (1,4,N)
-
-            var boxCorners = Functional.MatMul(slicedPositions[0, .., ..].Transpose(0,1), centersToCorners); // (N,4)
-            var indices = Functional.NMS(boxCorners, confidences[0,..], _iouThreshold, _scoreThreshold); // (N)
-
-            var classIds = Functional.Gather(argMaxClasses, 1, indices.Unsqueeze(0)).Unsqueeze(1); // (1,1,N)
-            var scores = Functional.Gather(confidences, 1, indices.Unsqueeze(0)).Unsqueeze(1); // (1,1,N)
-            var coords = Functional.IndexSelect(slicedPositions, 2, indices); // (1,4,N)
-
-            var concatenated = Functional.Concat(new FunctionalTensor[] { coords, classIds.Float(), scores }, 1); // (1,6,N)
-
-            _model = graph.Compile(concatenated);
-        }
     }
-
-    public class YOLOInferenceHandlerParameters
-    {
-        public bool AddClassificationHead { get; private set; }
-
-        public bool QuantizeModel { get; private set; }
-        public QuantizationType QuantizationType { get; private set; } = QuantizationType.Float16;
-        
-        public float IoUThreshold;
-        public float ScoreThreshold;
-        
-        public BackendType BackendType;
-
-        public YOLOInferenceHandlerParameters(YOLOCustomization yoloCustomization = YOLOCustomization.AddClassificationHead, YOLOQuantizationType yoloQuantizationType = YOLOQuantizationType.None, float iouThreshold = 0.5f, float scoreThreshold = 0.5f, BackendType backendType = BackendType.GPUCompute)
-        {
-            AddClassificationHead = yoloCustomization == YOLOCustomization.AddClassificationHead;
-            IoUThreshold = iouThreshold;
-            ScoreThreshold = scoreThreshold;
-            BackendType = backendType;
-
-            switch (yoloQuantizationType)
-            {
-                case YOLOQuantizationType.Float16:
-                    QuantizeModel = true;
-                    QuantizationType = QuantizationType.Float16;
-                    break;
-                case YOLOQuantizationType.Uint8:
-                    QuantizeModel = true;
-                    QuantizationType = QuantizationType.Uint8;
-                    break;
-                default:
-                    QuantizeModel = false;
-                    break;
-            }
-
-        }
-    }
-
-    public class YOLOModelAsset
-    {
-        public ModelAsset ModelAsset { get; private set; }
-
-        public YOLOModelAsset(ModelAsset modelAsset)
-        {
-            ModelAsset = modelAsset;
-        }
-    }
-
-    public enum YOLOQuantizationType
-    {
-        None,
-        Float16,
-        Uint8
-    }
-
-    public enum YOLOCustomization
-    { 
-        AddClassificationHead,
-        None
-    }
-
 }
