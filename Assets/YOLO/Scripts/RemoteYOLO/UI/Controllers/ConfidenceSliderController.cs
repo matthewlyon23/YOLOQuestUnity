ffcec10d4fe510f96d50df119db6e12fa36d7dd2
Matthew Lyon <lyonmatthew2003@hotmail.com>
2025-08-13 10:17:49 +0100

    Refactored UI Controllers

---
+++ Assets/YOLO/Scripts/RemoteYOLO/UI/Controllers/ConfidenceSliderController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Globalization;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+namespace YOLOQuestUnity.YOLO.RemoteYOLO.UI.Controllers
+{
+    public class ConfidenceSliderController : MonoBehaviour
+    {
+
+        [SerializeField] private TextMeshProUGUI textMeshProUGUI;
+        [SerializeField] private RemoteYOLOHandler remoteYoloHandler;
+
+        void Start()
+        {
+            gameObject.GetComponent<Slider>().value = (float)Math.Round(remoteYoloHandler.m_confidenceThreshold, 2);
+            textMeshProUGUI.text = Math.Round(remoteYoloHandler.m_confidenceThreshold, 2)
+                .ToString(CultureInfo.InvariantCulture);
+        }
+
+        public void OnConfidenceSliderValueChanged(Slider slider)
+        {
+            textMeshProUGUI.text = Math.Round(slider.value, 2).ToString(CultureInfo.InvariantCulture);
+            remoteYoloHandler.m_confidenceThreshold = (float)Math.Round(slider.value, 2);
+        }
+    }
+}
